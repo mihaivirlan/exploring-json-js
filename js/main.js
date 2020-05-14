f7ebc871e9048ad4ed2b5a33b67dcc381201ad4a
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 22:20:25 +0300

    Triple level JSON data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-var theData2 = '{"Jane":{"age":"29", "degree":"MFA"}, "Jim":{"age":"49", "degree":"MA"}}';
+var theData2 = '{"Jane":{"age":"29", "degree":{"AAS":"VMI", "BA":"UVA"}}, "Jim":{"age":"49", "degree":{"AAS":"VMI", "BA":"UVA"}}}';
 var myObj = JSON.parse(theData2);
 console.log(myObj);
 
 // document.getElementById('message').innerHTML = myObj.Jim.age;
-document.getElementById('message').innerHTML = myObj.Jane.age;
+document.getElementById('message').innerHTML = myObj.Jane.degree.BA;
