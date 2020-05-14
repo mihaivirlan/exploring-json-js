e272e12fa384f0cf997ca3d9ed20a2f2f7233fab
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-14 22:01:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var start = '{"favColor":"Orange", "favSeason":"Fall"}';
+
+var myObject = JSON.parse(start);
+console.log(myObject);
+
+var myString = JSON.stringify(myObject);
+console.log(myString);
+
+var myObject = JSON.parse(myString);
+console.log(myObject);
